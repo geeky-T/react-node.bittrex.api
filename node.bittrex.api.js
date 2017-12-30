ee5c5fa5ef9d88311e54729268dea753e0a96412
Tirth Patel <pateltirth001@gmail.com>
2017-12-30 17:32:33 +0530

    base version of react-node.bittrex.api

---
+++ node.bittrex.api.js
@@ -0,0 +1,230 @@
+/* ============================================================
+ * react-node.bittrex.api
+ * https://github.com/geeky-T/react-node.bittrex.api
+ * ============================================================ */
+
+const NodeBittrexApi = function() {
+  'use strict';
+
+  const hmac_sha512 = require('./hmac-sha512.js');
+
+  const default_request_options = {
+    method: 'GET',
+    agent: false,
+    headers: {
+      'User-Agent': 'Mozilla/4.0 (compatible; Node Bittrex API)',
+      'Content-type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  const opts = {
+    baseUrl: 'https://bittrex.com/api/v1.1',
+    baseUrlv2: 'https://bittrex.com/Api/v2.0',
+    websockets_baseurl: 'wss://socket.bittrex.com/signalr',
+    websockets_hubs: ['CoreHub'],
+    apikey: 'APIKEY',
+    apisecret: 'APISECRET',
+    verbose: false,
+    cleartext: false,
+    inverse_callback_arguments: false,
+    websockets: {
+      autoReconnect: true,
+    },
+    requestTimeoutInSeconds: 15,
+  };
+  var lastNonces = [];
+
+  var getNonce = function() {
+    var nonce = new Date().getTime();
+
+    if (lastNonces.indexOf(nonce) > -1) {
+      // we already used this nonce so keep trying to get a new one.
+      return getNonce();
+    }
+
+    // keep the last X to try ensure we don't have collisions even if the clock is adjusted
+    lastNonces = lastNonces.slice(-50);
+
+    lastNonces.push(nonce);
+
+    return nonce;
+  };
+
+  var extractOptions = function(options) {
+    var o = Object.keys(options),
+      i;
+    for (i = 0; i < o.length; i++) {
+      opts[o[i]] = options[o[i]];
+    }
+  };
+
+  var apiCredentials = function(uri) {
+    var options = {
+      apikey: opts.apikey,
+      nonce: getNonce()
+    };
+
+    return setRequestUriGetParams(uri, options);
+  };
+
+  var setRequestUriGetParams = function(uri, options) {
+    var op;
+    if (typeof(uri) === 'object') {
+      op = uri;
+      uri = op.uri;
+    } else {
+      op = Object.assign({}, default_request_options);
+    }
+
+
+    var o = Object.keys(options),
+      i;
+    for (i = 0; i < o.length; i++) {
+      uri = updateQueryStringParameter(uri, o[i], options[o[i]]);
+    }
+
+    op.headers.apisign = hmac_sha512.HmacSHA512(uri, opts.apisecret); // setting the HMAC hash `apisign` http header
+    op.uri = uri;
+    op.timeout = opts.requestTimeoutInSeconds * 1000;
+
+    return op;
+  };
+
+  var updateQueryStringParameter = function(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+    if (uri.match(re)) {
+      uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+      uri = uri + separator + key + "=" + value;
+    }
+
+    return uri;
+  };
+
+
+  var sendRequestCallback = function(callback, op) {
+    var start = Date.now();
+    console.log(op.uri);
+    fetch(op.uri)
+      .then((response) => response.json())
+      .then((result) => {
+        ((opts.verbose) ? console.log("requested from " + op.uri + " in: %ds", (Date.now() - start) / 1000) : '');
+          if (!result || !result.success) {
+            // error returned by bittrex API - forward the result as an error
+            return ((opts.inverse_callback_arguments) ?
+              callback(result, null) :
+              callback(null, result));
+          }
+          return ((opts.inverse_callback_arguments) ?
+            callback(null, ((opts.cleartext) ? JSON.stringify(result) : result)) :
+            callback(((opts.cleartext) ? JSON.stringify(result) : result), null));
+      });
+  };
+
+  var publicApiCall = function(url, callback, options) {
+    var op = Object.assign({}, default_request_options);
+    if (!options) {
+      op.uri = url;
+    }
+    sendRequestCallback(callback, (!options) ? op : setRequestUriGetParams(url, options));
+  };
+
+  var credentialApiCall = function(url, callback, options) {
+    if (options) {
+      options = setRequestUriGetParams(apiCredentials(url), options);
+    }
+    sendRequestCallback(callback, options);
+  };
+
+  return {
+    options: function(options) {
+      extractOptions(options);
+    },
+    sendCustomRequest: function(request_string, callback, credentials) {
+      var op;
+
+      if (credentials === true) {
+        op = apiCredentials(request_string);
+      } else {
+        op = Object.assign({}, default_request_options, { uri: request_string });
+      }
+      sendRequestCallback(callback, op);
+    },
+    getmarkets: function(callback) {
+      publicApiCall(opts.baseUrl + '/public/getmarkets', callback, null);
+    },
+    getcurrencies: function(callback) {
+      publicApiCall(opts.baseUrl + '/public/getcurrencies', callback, null);
+    },
+    getticker: function(options, callback) {
+      publicApiCall(opts.baseUrl + '/public/getticker', callback, options);
+    },
+    getmarketsummaries: function(callback) {
+      publicApiCall(opts.baseUrl + '/public/getmarketsummaries', callback, null);
+    },
+    getmarketsummary: function(options, callback) {
+      publicApiCall(opts.baseUrl + '/public/getmarketsummary', callback, options);
+    },
+    getorderbook: function(options, callback) {
+      publicApiCall(opts.baseUrl + '/public/getorderbook', callback, options);
+    },
+    getmarkethistory: function(options, callback) {
+      publicApiCall(opts.baseUrl + '/public/getmarkethistory', callback, options);
+    },
+    getcandles: function(options, callback) {
+      publicApiCall(opts.baseUrlv2 + '/pub/market/GetTicks', callback, options);
+    },
+    buylimit: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/market/buylimit', callback, options);
+    },
+    buymarket: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/market/buymarket', callback, options);
+    },
+    selllimit: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/market/selllimit', callback, options);
+    },
+    tradesell: function(options, callback) {
+      credentialApiCall(opts.baseUrlv2 + '/key/market/TradeSell', callback, options);
+    },
+    tradebuy: function(options, callback) {
+      credentialApiCall(opts.baseUrlv2 + '/key/market/TradeBuy', callback, options);
+    },
+    sellmarket: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/market/sellmarket', callback, options);
+    },
+    cancel: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/market/cancel', callback, options);
+    },
+    getopenorders: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/market/getopenorders', callback, options);
+    },
+    getbalances: function(callback) {
+      credentialApiCall(opts.baseUrl + '/account/getbalances', callback, {});
+    },
+    getbalance: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/getbalance', callback, options);
+    },
+    getwithdrawalhistory: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/getwithdrawalhistory', callback, options);
+    },
+    getdepositaddress: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/getdepositaddress', callback, options);
+    },
+    getdeposithistory: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/getdeposithistory', callback, options);
+    },
+    getorderhistory: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/getorderhistory', callback, options || {});
+    },
+    getorder: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/getorder', callback, options);
+    },
+    withdraw: function(options, callback) {
+      credentialApiCall(opts.baseUrl + '/account/withdraw', callback, options);
+    }
+  };
+}();
+
+module.exports = NodeBittrexApi;
